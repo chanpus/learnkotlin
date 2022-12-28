31bc8dca4b8d017b8f6c5a70966d8cc8e30b1380
chanpus <t1000v@naver.com>
2022-12-28 12:43:26 +0900

    test

---
+++ app/src/main/java/com/example/myapplication/saMple.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+fun main(){
+    println(max2(3 ,5))
+
+}
+
+
+fun max(a :Int,b:Int):Int{
+    if(a>b){
+     return a
+    }else{
+        return b
+    }
+}
+
+fun max2(a:Int,b:Int):Int = if(a>b) a else b
